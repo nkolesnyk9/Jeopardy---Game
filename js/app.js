c3dc3ca337589e2f5b6b38d892522a0cd2108207
Username <apkolesnyk94@gmail.com>
2022-07-16 16:25:38 -0400

    updates Jul16

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 const scoreWindow = document.getElementById("score")
 let score = 0
 
-
-
 const categoryWho = [
     
             {
@@ -77,105 +75,184 @@ const categoryWhen = [
             }
         ];
 
-
-
-        const forWho = () => {
+const forWho = () => {
             let points = 100
-            let whoContainer = document.querySelector(".questionWho")
+            let whoContainer = document.getElementById("who-container")
             let div = document.createElement("div")
             div.setAttribute("id","flexCardWho")
             
             categoryWho.forEach((element) => {
-                let card = document.createElement("card")
-                card.setAttribute("class",`${points}`)
+                let card = document.createElement("div")
+                card.setAttribute("class",`card who ${points}`)
                 let h3 = document.createElement("h3")
                 h3.innerHTML = element.question
                 card.appendChild(h3)
                 let buttonOne = document.createElement("button")
-                let buttonTwo = document.createElement("button")
+            buttonOne.setAttribute("class","buttonOne")
+            let buttonTwo = document.createElement("button")
+            buttonTwo.setAttribute("class","buttonTwo")
                 buttonOne.innerHTML = element.answers[0]
                 buttonTwo.innerHTML = element.answers[1]
                 points += 100
                 div.appendChild(card)
-                div.appendChild(buttonOne)
-                div.appendChild(buttonTwo)
+                card.appendChild(buttonOne)
+                card.appendChild(buttonTwo)
             })
            
             whoContainer.appendChild(div)
             
         }
-        forWho()
+forWho()
 
-        const forWhat = () => {
+const forWhat = () => {
             let points = 100
-            let whatContainer = document.querySelector(".questionWhat")
+            let whatContainer = document.getElementById("what-container")
             let div = document.createElement("div")
             div.setAttribute("id","flexCardWhat")
 
             categoryWhat.forEach((element) => {
-                let card = document.createElement("card")
-                card.setAttribute("class",`${points}`)
+                let card = document.createElement("div")
+                card.setAttribute("class",`card ${points}`)
                 let h3 = document.createElement("h3")
                 h3.innerHTML = element.question
                 card.appendChild(h3)
                 let buttonOne = document.createElement("button")
-                let buttonTwo = document.createElement("button")
+            buttonOne.setAttribute("class","buttonOne")
+            let buttonTwo = document.createElement("button")
+            buttonTwo.setAttribute("class","buttonTwo")
                 buttonOne.innerHTML = element.answers[0]
                 buttonTwo.innerHTML = element.answers[1]
                 points += 100
                 div.appendChild(card)
-                div.appendChild(buttonOne)
-                div.appendChild(buttonTwo)
+                card.appendChild(buttonOne)
+                card.appendChild(buttonTwo)
             })
 
             whatContainer.appendChild(div)
         }
-        forWhat()
+forWhat()
 
-        const forWhere = () => {
+const forWhere = () => {
             let points = 100
-            let whereContainer = document.querySelector(".questionWhere")
+            let whereContainer = document.getElementById("where-container")
             let div = document.createElement("div")
             div.setAttribute("id","flexCardWhere")
             categoryWhere.forEach((element) => {
-                let card = document.createElement("card")
-                card.setAttribute("class",`${points}`)
+                let card = document.createElement("div")
+                card.setAttribute("class",`card ${points}`)
                 let h3 = document.createElement("h3")
                 h3.innerHTML = element.question
                 card.appendChild(h3)
                 let buttonOne = document.createElement("button")
+                buttonOne.setAttribute("class","buttonOne")
                 let buttonTwo = document.createElement("button")
+                buttonTwo.setAttribute("class","buttonTwo")
                 buttonOne.innerHTML = element.answers[0]
                 buttonTwo.innerHTML = element.answers[1]
                 points += 100
                 div.appendChild(card)
-                div.appendChild(buttonOne)
-                div.appendChild(buttonTwo)
+                card.appendChild(buttonOne)
+                card.appendChild(buttonTwo)
         })
         whereContainer.appendChild(div)
     }
-    forWhere()
+forWhere()
 
-    const forWhen = () => {
+const forWhen = () => {
         let points = 100
-        let whenContainer = document.querySelector(".questionWhen")
+        let whenContainer = document.getElementById("when-container")
         let div = document.createElement("div")
         div.setAttribute("id","flexCardWhen")
         categoryWhere.forEach((element) => {
-            let card = document.createElement("card")
-            card.setAttribute("class",`${points}`)
+            let card = document.createElement("div")
+            card.setAttribute("class",`card ${points}`)
             let h3 = document.createElement("h3")
             h3.innerHTML = element.question
             card.appendChild(h3)
             let buttonOne = document.createElement("button")
+            buttonOne.setAttribute("class","buttonOne")
             let buttonTwo = document.createElement("button")
+            buttonTwo.setAttribute("class","buttonTwo")
             buttonOne.innerHTML = element.answers[0]
             buttonTwo.innerHTML = element.answers[1]
             points += 100
+            card.appendChild(buttonOne)
+            card.appendChild(buttonTwo)
             div.appendChild(card)
-            div.appendChild(buttonOne)
-            div.appendChild(buttonTwo)
     })
     whenContainer.appendChild(div)
 }
 forWhen()
+
+
+
+// const flipCard = () => {
+//     let backCard = document.getElementsByClassName("card")//[div.card.100,div.card.100, etc]
+//     console.log(backCard)
+//     let backOfCard = document.createElement("div")
+//     backOfCard.setAttribute("class", "backOfCard")
+//     console.log(backOfCard)
+
+    
+//     backCard.appendChild(backOfCard)
+    
+// }
+// flipCard()
+
+const flipCardWho = () => {
+    let points = 100
+            let whoCards= document.getElementById("flexCardWho")
+            console.log(whoCards)
+            let div = document.createElement("div")
+            div.setAttribute("id","backCardWho")
+            let cardWho = document.getElementsByClassName("who")
+            console.log(cardWho)
+
+            categoryWho.forEach((element) => {
+                let card = document.createElement("div")
+                card.setAttribute("class",`backCard ${points}`)
+                let h3 = document.createElement("h3")
+                h3.innerHTML = points 
+                points += 100
+                card.appendChild(h3)
+                div.appendChild(card)
+
+            })
+
+           whoCards.appendChild(div)
+}
+flipCardWho()
+
+
+
+
+
+// const flipCardForWhat = () => {
+//     let backOfWhat = document.getElementById("flexCardWhat")
+//     let backCard = document.createElement("div")
+//     backCard.setAttribute("class", "backCard")
+//     console.log(backCard)
+//     console.log(backOfWhat)
+//     backOfWhat.appendChild(backCard)
+// }
+// flipCardForWhat()
+
+// const flipCardForWhere = () => {
+//     let backOfWhere = document.getElementById("flexCardWhere")
+//     let backCard = document.createElement("div")
+//     backCard.setAttribute("class", "backCard")
+//     console.log(backCard)
+//     console.log(backOfWhere)
+//     backOfWhere.appendChild(backCard)
+// }
+// flipCardForWhere()
+
+// const flipCardForWhen = () => {
+//     let backOfWhen = document.getElementById("flexCardWhen")
+//     let backCard = document.createElement("div")
+//     backCard.setAttribute("class", "backCard")
+//     console.log(backCard)
+//     console.log(backOfWhen)
+//     backOfWhen.appendChild(backCard)
+// }
+// flipCardForWhen
