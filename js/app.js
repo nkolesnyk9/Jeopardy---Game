59f7cb224214614ebb98cb19f5409a23b87b8353
Username <apkolesnyk94@gmail.com>
2022-07-15 20:51:15 -0400

    updatesJul15

--- js/app.js
+++ js/app.js
@@ -1,20 +1,10 @@
 const scoreWindow = document.getElementById("score")
 let score = 0
 
-const forWho = () => {
-    let whoContainer = document.getElementById("who-container")
-    let card = document.getElementById("flexCard")
-    for(let element of question) {
-        card.innerHTML = element
-    }
-    whoContainer.appendChild(card)
-}
-forWho()
+
 
 const categoryWho = [
-    {
-        category:"Who",
-        questions:[
+    
             {
                 question: 'Who wrote the Harry Potter books?',
                 answers: ['JK Rowling', 'JRR Tolkien'],
@@ -30,16 +20,12 @@ const categoryWho = [
                 answers: ['Carl Benz', 'Henry Ford'],
                 correct: 'Carl Benz',
             },
-                ],
-            },
-        ]  
+                ];
+      
         
 const categoryWhat = [ 
-    {
-        category:"What",
-        questions:[
             {
-                question: 'Who color is Ukrainian flag?',
+                question: 'What color is Ukrainian flag?',
                 answers: ['blue & yellow', 'red,blue & white'],
                 correct: 'blue & yellow',
             },
@@ -52,38 +38,28 @@ const categoryWhat = [
                 question: 'What do Koalas eat?',
                 answers: ['Straw', 'Eucalypt'],
                 correct: 'Eucalypt',
-            },
-        ],
-    }
-]
-    
+            }
+        ];
+ 
 const categoryWhere = [ 
-    {
-        category:"Where",
-        questions:[
             {
-                questionOne: 'Where is Central Park?',
-                answersOne: ['New York', 'Chicago'],
+                question: 'Where is Central Park?',
+                answers: ['New York', 'Chicago'],
                 correct: 'New York',
             },
             {
-                questionTwo: 'Where is Coliseum?',
-                answersTwo: ['Milan', 'Rome'],
+                question: 'Where is Coliseum?',
+                answers: ['Milan', 'Rome'],
                 correct: 'Rome',
             },
             {
-                questionThree: 'Where is Mount Kilamanjaro?',
-                answersThree: ['Zimbabwe', 'Tanzania'],
+                question: 'Where is Mount Kilamanjaro?',
+                answers: ['Zimbabwe', 'Tanzania'],
                 correct: 'Tanzania',
-            },
-        ]
-    }
-]
-    
+            }
+        ];  
+
 const categoryWhen = [ 
-        {
-        category:"When",
-        questions:[
             {
                 question: 'When is Christmas?',
                 answers: ['30th Dec', '24th/25th Dec'],
@@ -98,10 +74,108 @@ const categoryWhen = [
                 question: 'When JFK was shot?',
                 answers: ['1963', '1964'],
                 correct: '1963',
-            },
-        ],
-
             }
-        ]
-    
+        ];
 
+
+
+        const forWho = () => {
+            let points = 100
+            let whoContainer = document.querySelector(".questionWho")
+            let div = document.createElement("div")
+            div.setAttribute("id","flexCardWho")
+            
+            categoryWho.forEach((element) => {
+                let card = document.createElement("card")
+                card.setAttribute("class",`${points}`)
+                let h3 = document.createElement("h3")
+                h3.innerHTML = element.question
+                card.appendChild(h3)
+                let buttonOne = document.createElement("button")
+                let buttonTwo = document.createElement("button")
+                buttonOne.innerHTML = element.answers[0]
+                buttonTwo.innerHTML = element.answers[1]
+                points += 100
+                div.appendChild(card)
+                div.appendChild(buttonOne)
+                div.appendChild(buttonTwo)
+            })
+           
+            whoContainer.appendChild(div)
+            
+        }
+        forWho()
+
+        const forWhat = () => {
+            let points = 100
+            let whatContainer = document.querySelector(".questionWhat")
+            let div = document.createElement("div")
+            div.setAttribute("id","flexCardWhat")
+
+            categoryWhat.forEach((element) => {
+                let card = document.createElement("card")
+                card.setAttribute("class",`${points}`)
+                let h3 = document.createElement("h3")
+                h3.innerHTML = element.question
+                card.appendChild(h3)
+                let buttonOne = document.createElement("button")
+                let buttonTwo = document.createElement("button")
+                buttonOne.innerHTML = element.answers[0]
+                buttonTwo.innerHTML = element.answers[1]
+                points += 100
+                div.appendChild(card)
+                div.appendChild(buttonOne)
+                div.appendChild(buttonTwo)
+            })
+
+            whatContainer.appendChild(div)
+        }
+        forWhat()
+
+        const forWhere = () => {
+            let points = 100
+            let whereContainer = document.querySelector(".questionWhere")
+            let div = document.createElement("div")
+            div.setAttribute("id","flexCardWhere")
+            categoryWhere.forEach((element) => {
+                let card = document.createElement("card")
+                card.setAttribute("class",`${points}`)
+                let h3 = document.createElement("h3")
+                h3.innerHTML = element.question
+                card.appendChild(h3)
+                let buttonOne = document.createElement("button")
+                let buttonTwo = document.createElement("button")
+                buttonOne.innerHTML = element.answers[0]
+                buttonTwo.innerHTML = element.answers[1]
+                points += 100
+                div.appendChild(card)
+                div.appendChild(buttonOne)
+                div.appendChild(buttonTwo)
+        })
+        whereContainer.appendChild(div)
+    }
+    forWhere()
+
+    const forWhen = () => {
+        let points = 100
+        let whenContainer = document.querySelector(".questionWhen")
+        let div = document.createElement("div")
+        div.setAttribute("id","flexCardWhen")
+        categoryWhere.forEach((element) => {
+            let card = document.createElement("card")
+            card.setAttribute("class",`${points}`)
+            let h3 = document.createElement("h3")
+            h3.innerHTML = element.question
+            card.appendChild(h3)
+            let buttonOne = document.createElement("button")
+            let buttonTwo = document.createElement("button")
+            buttonOne.innerHTML = element.answers[0]
+            buttonTwo.innerHTML = element.answers[1]
+            points += 100
+            div.appendChild(card)
+            div.appendChild(buttonOne)
+            div.appendChild(buttonTwo)
+    })
+    whenContainer.appendChild(div)
+}
+forWhen()
